c0b1f0575e21dc33cf165ee56f5d435b3ead3792
aluco <info@aluco.jp>
2019-10-04 19:07:06 +0900

    レスポンシブの調整 スライダーの部分とハンバーガーメニューの実装がまだです。（スライダー部分を消せば余白はでないです） フッターの下の余白はまた次回やります

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 $(function(){
 
+  $('.center').slick({
+    arrows:false,
+    swipeToslide:true,
+    adaptiveHeight:false,
+    infinite: true, //スライドのループ有効化
+    dots: false, //ドットのナビゲーションを表示
+    centerPadding: '15%', //両サイドの見えている部分のサイズ
+    currentMode:true,
+    autoplay: true, //自動再生
+    variableWidth: true,
+    responsive: [{
+      breakpoint: 750, // 600〜1023px
+      settings: {
+        variableWidth: false,
+        centerMode: true, //要素を中央寄せ
+        slidesToShow:1,
+      }
+    }]
+  });
+
+
    $('.multiple').slick({
      arrows:false,
       autoplay: false, //自動再生
-
       infinite: true, //スライドのループ有効化
       dots: false, //ドットのナビゲーションを表示
       autoplaySpeed: 0,
-   cssEase: 'linear',
-   speed: 10000,
-      slidesToShow: 4, //表示するスライドの数
-      slidesToScroll: 4, //スクロールで切り替わるスライドの数
+     cssEase: 'linear',
+     speed: 10000,
+      // slidesToShow: 4, //表示するスライドの数
+      // slidesToScroll: 4, //スクロールで切り替わるスライドの数
       responsive: [{
         breakpoint: 768, //ブレークポイントが768px
-        settings: {
-          slidesToShow: 3, //表示するスライドの数
-          slidesToScroll: 3, //スクロールで切り替わるスライドの数
-        }
-      }, {
-        breakpoint: 480, //ブレークポイントが480px
         settings: {
           slidesToShow: 2, //表示するスライドの数
-          slidesToScroll: 2, //スクロールで切り替わるスライドの数
-        }
-      }]
-    });   $('.multiple').slick({
-      arrows:false,
-      autoplay: true, //自動再生
-      infinite: true, //スライドのループ有効化
-      dots: false, //ドットのナビゲーションを表示
-      slidesToShow: 4, //表示するスライドの数
-      slidesToScroll: 4, //スクロールで切り替わるスライドの数
-      responsive: [{
-        breakpoint: 768, //ブレークポイントが768px
-        settings: {
-          slidesToShow: 3, //表示するスライドの数
-          slidesToScroll: 3, //スクロールで切り替わるスライドの数
+          slidesToScroll: 1, //スクロールで切り替わるスライドの数
         }
       }, {
         breakpoint: 480, //ブレークポイントが480px
@@ -45,5 +45,7 @@ $(function(){
         }
       }]
     });
+
+
 
 });
