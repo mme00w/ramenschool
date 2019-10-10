52b3654fc08eeed2f6226eb9305c393a08cdfd8c
aluco <info@aluco.jp>
2019-10-10 19:08:11 +0900

    pixelperfectで細かな調整 slickの調整（矢印画像の表示がどうしてもできない） ハンバーガーメニューの実装がまだ

--- js/script.js
+++ js/script.js
@@ -1,50 +1,62 @@
+// $(function(){
+//   $(".voice > .center").slick({
+//     prevArrow: '<img src="../images/voice-arrow-r.png" class="slide-arrow prev-arrow">',
+//     nextArrow: '<img src="../images/voice-arrow-l.png" class="slide-arrow next-arrow">'
+//   });
+// });
+
 $(function(){
-
-  $('.center').slick({
-    arrows:false,
-    swipeToslide:true,
-    adaptiveHeight:false,
+  $('.hero .center').slick({
     infinite: true, //スライドのループ有効化
-    dots: false, //ドットのナビゲーションを表示
-    centerPadding: '15%', //両サイドの見えている部分のサイズ
-    currentMode:true,
-    autoplay: true, //自動再生
-    variableWidth: true,
-    responsive: [{
-      breakpoint: 750, // 600〜1023px
+    dots:false, //ドットのナビゲーションを表示
+    centerMode: true, //要素を中央寄せ
+    slidesToShow: 4,
+    centerPadding:'10%', //両サイドの見えている部分のサイズ
+    autoplay:true, //自動再生
+    responsive: [
+    {
+      breakpoint: 751,
       settings: {
-        variableWidth: false,
-        centerMode: true, //要素を中央寄せ
-        slidesToShow:1,
+        slidesToShow: 3,
+        centerMode: false,
       }
-    }]
-  });
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 2,
+        centerMode: false,
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 
-   $('.multiple').slick({
-     arrows:false,
-      autoplay: false, //自動再生
-      infinite: true, //スライドのループ有効化
-      dots: false, //ドットのナビゲーションを表示
-      autoplaySpeed: 0,
-     cssEase: 'linear',
-     speed: 10000,
-      // slidesToShow: 4, //表示するスライドの数
-      // slidesToScroll: 4, //スクロールで切り替わるスライドの数
-      responsive: [{
-        breakpoint: 768, //ブレークポイントが768px
-        settings: {
-          slidesToShow: 2, //表示するスライドの数
-          slidesToScroll: 1, //スクロールで切り替わるスライドの数
-        }
-      }, {
-        breakpoint: 480, //ブレークポイントが480px
-        settings: {
-          slidesToShow: 2, //表示するスライドの数
-          slidesToScroll: 2, //スクロールで切り替わるスライドの数
-        }
-      }]
-    });
+  $('.voice .center').slick({
+    infinite: true, //スライドのループ有効化
+    dots:true, //ドットのナビゲーションを表示
+    centerMode: true, //要素を中央寄せ
+    slidesToShow: 3,
+    centerPadding:'10%', //両サイドの見えている部分のサイズ
+    autoplay:false, //自動再生
+    responsive: [
+    {
+      breakpoint: 751,
+      settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding:'20%',
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
 
 
 
