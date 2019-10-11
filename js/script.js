bbf2de724218a011cec4c0d7d923499735f9a7c6
aluco <info@aluco.jp>
2019-10-11 19:09:41 +0900

    改善箇所は全て改善しましたが、CSSの並び替えがまだ途中。（divを指定しないなどはできましたが、順序がまだ綺麗に整えきれてません） 改善途中にblogが見えなくなってしまいましたがそれは次回やります。

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ $(function(){
     // instead of a settings object
   ]
 });
-$('.voice .center').slick({
+$('.voices .center').slick({
   infinite: true, //スライドのループ有効化
   dots:true, //ドットのナビゲーションを表示
   centerMode: true, //要素を中央寄せ
